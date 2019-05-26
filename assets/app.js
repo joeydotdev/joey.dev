a079943e4a7fc763bd370677f3d4765e257ce963
odxs <odus@knights.ucf.edu>
2019-05-25 18:26:51 -0700

    Fix mobile terminal keyboard bug

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,7 @@
 const BLACKLISTED_KEY_CODES = [ 38 ];
 const COMMANDS = {
     help: "Supported commands: <span class=\"code\">about</span>, <span class=\"code\">experience</span>, <span class=\"code\">education</span>, <span class=\"code\">skills</span>, <span class=\"code\">corgi</span>",
-    about: "Hello 👋<br>As the domain suggets, my name is Joey. I’m a full-stack developer currently living in Orlando, FL as I attend the University of Central Florida. My passion for what has me in this field is the limitless potential to positively impact other people’s lives with what I create. Building something and then having people use my creation is the coolest thing ever to me. Outside of coding, you can definitely find me looking at dog *corgi* pictures :)",
+    about: "Hello 👋<br>As the domain suggests, my name is Joey. I’m a full-stack developer currently living in Orlando, FL as I attend the University of Central Florida. My passion for what has me in this field is the limitless potential to positively impact other people’s lives with what I create. Building something and then having people use my creation is the coolest thing ever to me. Outside of coding, you can definitely find me looking at dog *corgi* pictures :)",
     skills: "<span class=\"code\">Languages:</span> JavaScript, TypeScript, PHP, Java, Python, C<br><span class=\"code\">Technologies:</span> Node.js, Git, Linux, NGINX, Apache, MySQL<br><span class=\"code\">Frameworks:</span> Angular, React.js, Vue.js, Laravel, Flask, Bootstrap, Selenium",
     education: "University of Central Florida<br>B.S. Information Technology — Emphasis in Software Engineering",
     resume: "<a href='./joey_colon_resume.pdf' class='success link'>resume.pdf</a>",
@@ -13,6 +13,7 @@ let userInput, terminalOutput;
 const app = () => {
     userInput = document.getElementById('userInput');
     terminalOutput = document.getElementById('terminalOutput');
+    document.getElementById('dummyKeyboard').focus();
     console.log('Application loaded');
 };
 
