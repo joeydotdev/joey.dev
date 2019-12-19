a6adeea627f96d58dbf281626e90b61bd484608f
unknown <contact@joey.dev>
2019-12-19 15:55:28 -0500

    w/e

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,11 @@
 const BLACKLISTED_KEY_CODES = [ 38 ];
 const COMMANDS = {
     help: "Supported commands: <span class=\"code\">about</span>, <span class=\"code\">experience</span>, <span class=\"code\">education</span>, <span class=\"code\">skills</span>, <span class=\"code\">corgi</span>",
-    about: "Hello 👋<br>As the domain suggests, my name is Joey. I’m a full-stack developer currently living in Orlando, FL as I attend the University of Central Florida. I have a burning passion to want and help others with code that I create. I enjoy the limitless potential of impact that I can have with what I build. It is what pushes me every day to become a better engineer. Outside of coding, you can find me looking at corgis on Instagram!",
-    skills: "<span class=\"code\">Languages:</span> JavaScript, TypeScript, PHP, Java, Python, C<br><span class=\"code\">Technologies:</span> Node.js, Git, Docker, Linux, NGINX, Apache, MySQL<br><span class=\"code\">Frameworks:</span> Angular, React.js, Vue.js, Laravel, Flask, Bootstrap, Selenium",
-    education: "University of Central Florida<br>B.S. Information Technology — Emphasis in Software Engineering",
+    about: "Hiya 👋 <br>I'm a rising senior at UCF with a passion for wanting to help others. I am interested in full-stack/backend engineering and infrastructure.",
+    skills: "<span class=\"code\">Languages:</span> JavaScript, TypeScript, PHP, Java, Go, C",
+    education: "<strong class=\"header-name\">University of Central Florida</strong><br>B.S. Information Technology — Emphasis in Software Engineering",
     resume: "<a href='./joey_colon_resume.pdf' class='success link'>resume.pdf</a>",
-    experience: "LSQ Funding Group (Jan. 2019 - April 2019) (Orlando, FL)<br>Software Engineer Intern",
+    experience: "<strong class=\"header-name\">Uber (May 2020 - Aug. 2020)</strong><br><i>Software Engineering Intern</i><br><strong class=\"header-name\">Honey (Jan. 2020 - April 2020)</strong><br><i>Software Engineering Intern</i><br> <strong class=\"header-name\">LSQ (Jan. 2019 - April 2019)</strong> <br><i>Software Engineering Intern</i>",
     corgi: "My top 3 favorite corgis (click to view):<br><a href='https://www.instagram.com/bearorcorgi/' class='success link'>Bear</a>, <a href='https://www.instagram.com/lychee_the_corgi/' class='success link'>Mochee</a>, <a href='https://www.instagram.com/thecorgijack/' class='success link'>Jack</a>"
 };
 let userInput, terminalOutput;
