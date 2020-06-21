57f1b7055035abec4cae027243686ee948450460
= <odus@knights.ucf.edu>
2020-06-20 22:45:11 -0400

    goodbye to everybody who hated on me

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,11 @@
 const BLACKLISTED_KEY_CODES = [ 38 ];
 const COMMANDS = {
     help: "Supported commands: <span class=\"code\">about</span>, <span class=\"code\">experience</span>, <span class=\"code\">education</span>, <span class=\"code\">skills</span>, <span class=\"code\">corgi</span>",
-    about: "Hiya 👋 <br>I'm a rising senior at UCF with a passion for wanting to help others. I am interested in full-stack/backend engineering and infrastructure.",
-    skills: "<span class=\"code\">Languages:</span> JavaScript, TypeScript, PHP, Java, Go, C",
+    about: "Hiya 👋 <br>I'm a Software Engineer originally from Orlando, FL. I love corgis and I am particularly interested in building products from the ground up.",
+    skills: "<span class=\"code\">Languages:</span> JavaScript, TypeScript, PHP, Java, Python, C, HTML, CSS",
     education: "<strong class=\"header-name\">University of Central Florida</strong><br>B.S. Information Technology — Emphasis in Software Engineering",
     resume: "<a href='./joey_colon_resume.pdf' class='success link'>resume.pdf</a>",
-    experience: "<strong class=\"header-name\">Uber (May 2020 - Aug. 2020)</strong><br><i>Software Engineering Intern</i><br><strong class=\"header-name\">Honey (Jan. 2020 - April 2020)</strong><br><i>Software Engineering Intern</i><br> <strong class=\"header-name\">LSQ (Jan. 2019 - April 2019)</strong> <br><i>Software Engineering Intern</i>",
+    experience: "<strong class=\"header-name\">Uber (May 2020 - Aug. 2020)</strong><br><i>Software Engineering Intern</i><br><strong class=\"header-name\">Honey (Jan. 2020 - Mar. 2020)</strong><br><i>Software Engineering Intern</i><br> <strong class=\"header-name\">LSQ (Jan. 2019 - April 2019)</strong> <br><i>Software Engineering Intern</i>",
     corgi: "My top 3 favorite corgis (click to view):<br><a href='https://www.instagram.com/bearorcorgi/' class='success link'>Bear</a>, <a href='https://www.instagram.com/lychee_the_corgi/' class='success link'>Mochee</a>, <a href='https://www.instagram.com/thecorgijack/' class='success link'>Jack</a>"
 };
 let userInput, terminalOutput;
